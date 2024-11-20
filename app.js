5981719907794ce584efa3ff3295928e98d48fe7
rajen <rajendram9516@gmail.com>
2024-11-20 15:38:17 +0530

    checking

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const port=process.env.PORT || 3000
 app.get('/',(req,res)=>{
     res.send("done")
 })
+
+app.get("/user",(req,res)=>{
+    res.send("user")
+})
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`)
 })

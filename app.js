9f49b70a5972cbc763e13c72b1dc515604f77410
rajen <rajendram9516@gmail.com>
2024-11-20 18:50:15 +0530

    create sepret router folder to handle router

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const port=process.env.PORT || 3000
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/user",)
 
 app.set("view engine",'ejs')
 
@@ -14,9 +15,6 @@ app.get('/',(req,res)=>{
     res.send("done")
 })
 
-app.get("/signup",(req,res)=>{
-    res.render("signup")
-})
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`)
 })

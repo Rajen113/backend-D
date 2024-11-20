ae716b759bc23f61e4c30cfa94a0d2995c8c19da
rajen <rajendram9516@gmail.com>
2024-11-20 15:16:36 +0530

    practice for deployment

---
+++ app.js
@@ -0,0 +1,11 @@
+const express=require("express")
+const app=express()
+require('dotenv').config()
+const port=process.env.PORT || 3000
+
+app.get('/',(req,res)=>{
+    res.send("done")
+})
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
+})

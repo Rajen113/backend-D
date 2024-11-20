206eb943a810da097495e465e417e6a7c590c98e
rajen <rajendram9516@gmail.com>
2024-11-20 18:36:37 +0530

    add tailwind css

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express=require("express")
 const app=express()
+const path = require("path");
 require('dotenv').config()
 const port=process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set("view engine",'ejs')
 

e266994531386e66a77151383402b478ec09c21b
rajen <rajendram9516@gmail.com>
2024-11-20 16:23:35 +0530

    add ejs for frontend

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const app=express()
 require('dotenv').config()
 const port=process.env.PORT || 3000
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.set("view engine",'ejs')
+
 app.get('/',(req,res)=>{
     res.send("done")
 })
 
-app.get("/user",(req,res)=>{
-    res.send("user")
+app.get("/signup",(req,res)=>{
+    res.render("signup")
 })
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`)
